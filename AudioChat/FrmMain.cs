250d55275c2bba95c38135cac4ce826e43f780cb
李宝鑫 <libaoxin131521@hotmail.com>
2023-06-28 17:34:56 +0800

    checkin

--- AudioChat/FrmMain.cs
+++ AudioChat/FrmMain.cs
@@ -3,6 +3,9 @@ using IronPython.Hosting;
 using IronPython.Runtime.Operations;
 using Microsoft.Scripting.Hosting;
 using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Core.DevToolsProtocolExtension;
+using Microsoft.Web.WebView2.WinForms;
+using Microsoft.Web.WebView2.Wpf;
 using NAudio.Wave;
 using Newtonsoft.Json;
 using System;
@@ -14,51 +17,32 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
 
 namespace AudioChat
 {
     public partial class FrmMain : Form
     {
+        DevToolsProtocolHelper helper;
         public FrmMain()
         {
+
             InitializeComponent();
         }
 
         private async void FrmMain_Load(object sender, EventArgs e)
         {
-            webView21.Source = new Uri("https://chat.openai.com/c/ef83ba76-b6f2-4e7e-b840-cf132799fd6c");
             await webView21.EnsureCoreWebView2Async();
+
+            webView21.Source = new Uri("https://chat.openai.com/c/ef83ba76-b6f2-4e7e-b840-cf132799fd6c");
+
             webView21.CoreWebView2.WebResourceResponseReceived += CoreWebView2_WebResourceResponseReceived;
-            webView21.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
-            
-        }
 
-        List<string> responseMessage = new List<string>();
-        private void CoreWebView2_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
-        {
-            string message = e.TryGetWebMessageAsString();
-
-            Console.WriteLine(message);
-
-            if (message.Contains("[DONE]"))
-            {
-                string allContext = responseMessage[responseMessage.Count - 1];
-                allContext = allContext.Remove(0, 6);
-
-                responseMessage.Clear();
-                var gptMessage = JsonConvert.DeserializeObject<ChatGPTMessage>(allContext);
-
-                foreach (var item in gptMessage.content.parts)
-                {
-                    //await TextToVoice(item);
-                }
-            }
-            else
-            {
-                responseMessage.Add(message);
-            }
+            //helper = webView21.CoreWebView2.GetDevToolsProtocolHelper();
+            //await helper.Network.EnableAsync();
         }
 
         private async void CoreWebView2_WebResourceResponseReceived(object sender, CoreWebView2WebResourceResponseReceivedEventArgs e)
@@ -72,11 +56,12 @@ namespace AudioChat
                         StreamReader streamReader = new StreamReader(data);
                         string context = streamReader.ReadToEnd();
 
-                        Console.WriteLine("<<" + context + ">>");
+                        System.Console.WriteLine("<<" + context + ">>");
                     }
                     else
                     {
-                        Console.WriteLine("data is null");
+                        //TODO:获取内容
+                        //document.getElementsByClassName("markdown")[58].innerText
                     }
                 }
             }
@@ -84,7 +69,7 @@ namespace AudioChat
 
         private async void btnStart_Click(object sender, EventArgs e)
         {
-
+            await TextToVoice("测试一下播放");
         }
 
         private void BtnSound_Click(object sender, EventArgs e)
@@ -99,7 +84,7 @@ namespace AudioChat
             var result = await etts.SynthesisAsync(text, voices.Split('|').FirstOrDefault());
             if (result.Code != ResultCode.Success)
             {
-                Console.WriteLine("生成失败");
+                System.Console.WriteLine("生成失败");
                 return;
             }
 
